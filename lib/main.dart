7ad3f7c11502462ed5e39e27ef9e41a8a27b7434
Arshad Shaikh <105237701+Arshadshaykh@users.noreply.github.com>
2024-02-19 18:14:52 +0530

    more changes

--- lib/main.dart
+++ lib/main.dart
@@ -45,67 +45,11 @@ class _MyPageViewState extends State<MyPageView>
   bool hasAnimated = false;
   Animation<double>? animationtwo;
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 2));
-    animation = Tween<double>(begin: 10, end: 250).animate(CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeInOutBack,
-      // reverseCurve: Curves.bounceIn,
-    ));
-    animationtwo = Tween<double>(begin: 200, end: 10).animate(CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeInOutBack,
-      // reverseCurve: Curves.bounceIn,
-    ));
-    if (!hasAnimated) {
-      controller.forward();
-      hasAnimated = true;
-    }
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    controller.dispose();
-  }
-
-  final List<OnboardingItem> onboardingItems = [
-    OnboardingItem(
-      title: 'Welcome to MyApp',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      imagePath: 'assets/onboarding1.png',
-    ),
-    OnboardingItem(
-      title: 'Explore Features',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      imagePath: 'assets/onboarding2.png',
-    ),
-    OnboardingItem(
-      title: 'Get Started',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      imagePath: 'assets/onboarding3.png',
-    ),
-  ];
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: MyHomePage(),
-        // PageView.builder(
-        //   itemCount: 4,
-        //   itemBuilder: (context, index) {
-        // return MyHomePage(index: index);
-        // },
-        //   children: [
-        //   MyHomePage(),
-        //   Center(child: Text('2 nd')),
-        //   Center(child: Text('3 nd')),
-        // ],
       ),
       // ),
     );
@@ -163,18 +107,12 @@ class _MyHomePageState extends State<MyHomePage>
       body: Column(
         children: [
           Expanded(
-            child: Stack(
-              fit: StackFit.loose,
-              children: [
-                
-                PageView.builder(
-                  controller: _controller,
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    return OnboardingImage(context,index);
-                  },
-                ),
-              ],
+            child: PageView.builder(
+              controller: _controller,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return OnboardingImage(context,index);
+              },
             ),
           ),
         ],
@@ -186,144 +124,7 @@ class _MyHomePageState extends State<MyHomePage>
 
 
 Widget OnboardingImage(context, index) {
-  return Stack(
-    alignment: Alignment.center,
-    fit: StackFit.loose,
-    children: [
-
-      SizedBox(height: 426,width: 380,child: AvailableCreditComponent(index: index,)),
-      // buildImageWidget(
-      //         index: index,
-      //         imagePath: index == 0
-      //             ? OBmesseges
-      //             : index == 1
-      //                 ? OBmobile2
-      //                 : index == 2
-      //                     ? OBbook
-      //                     : OBmobile4,
-      //         top: index == 0
-      //             ? MediaQuery.of(context).size.height / 5.5
-      //             : index == 1
-      //                 ? MediaQuery.of(context).size.height / 2.3
-      //                 : index == 2
-      //                     ? MediaQuery.of(context).size.height / 2.1
-      //                     : MediaQuery.of(context).size.height / 3.2,
-      //         left: index == 2
-      //             ? MediaQuery.of(context).size.width / 21
-      //             : MediaQuery.of(context).size.width / 13,
-      //         height: index == 0
-      //             ? 270
-      //             : index == 1
-      //                 ? 168
-      //                 : index == 2
-      //                     ? 180
-      //                     : 325,
-      //         context: context)
-      //     .animate()
-      //     .slide(
-      //         duration: Duration(seconds: index == 0 ? 0 : 1),
-      //         begin: index == 1 ? Offset(-1, 0) : Offset(0, 1))
-      //     .fade(delay: Duration(milliseconds: 400))
-      //     .scale(duration: Duration(seconds: index == 0 ? 1 : 0)),
-
-      // /*
-      // 1: mobile
-      // 2: man2
-      // 3: Top books
-      // 4: man4
-      // */
-      // buildImageWidget(
-      //         index: index,
-      //         imagePath: index == 0
-      //             ? OBphone
-      //             : index == 1
-      //                 ? OBman2
-      //                 : index == 2
-      //                     ? OBbook2
-      //                     : OBman4,
-      //         top: OBphone
-      //             ? MediaQuery.of(context).size.height / 4.1
-      //             : index == 1
-      //                 ? MediaQuery.of(context).size.height / 4
-      //                 : index == 2
-      //                     ? MediaQuery.of(context).size.height / 2.58
-      //                     : MediaQuery.of(context).size.height / 2.5,
-      //         left: index == 0
-      //             ? MediaQuery.of(context).size.width / 3
-      //             : index == 1
-      //                 ? MediaQuery.of(context).size.width / 4
-      //                 : index == 2
-      //                     ? MediaQuery.of(context).size.width / 6
-      //                     : MediaQuery.of(context).size.width / 22,
-      //         height: index == 0
-      //             ? 270
-      //             : index == 1
-      //                 ? 208
-      //                 : index == 2
-      //                     ? 139
-      //                     : 250,
-      //         context: context)
-      //     .animate()
-      //     .slide(
-      //       duration: Duration(seconds:index==3?0: 1),
-      //     ),
-      // // .fade(delay: Duration(milliseconds: 400)),
-      // /*
-      // 1: woman1
-      // 2: Flower Pot
-      // 3: woman3
-      // 4: woman4
-      // */
-      // buildImageWidget(
-      //         index: index,
-      //         imagePath: index == 0
-      //             ? OBwoman
-      //             : index == 1
-      //                 ? OBflowerPot
-      //                 : index == 2
-      //                     ? OBwoman2
-      //                     : OBwoman4,
-      //         top: index == 0
-      //             ? MediaQuery.of(context).size.height / 2.33
-      //             : index == 1
-      //                 ? MediaQuery.of(context).size.height / 2.02
-      //                 : index == 2
-      //                     ? MediaQuery.of(context).size.height / 3.6
-      //                     : MediaQuery.of(context).size.height / 2.7,
-      //         left: index == 0
-      //             ? MediaQuery.of(context).size.width / 13
-      //             : index == 1
-      //                 ? MediaQuery.of(context).size.width / 1.2
-      //                 : index == 2
-      //                     ? MediaQuery.of(context).size.width / 2.05
-      //                     : MediaQuery.of(context).size.width / 2.5,
-      //         height: index == 0
-      //             ? 158
-      //             : index == 1
-      //                 ? 118
-      //                 : index == 2
-      //                     ? 245
-      //                     : 224,
-      //         context: context)
-      //     .animate()
-      //     .slide(duration: Duration(seconds: 1))
-      //     // .scale(
-      //     //     begin: Offset(
-      //     //       index == 1 ? 0 : 0,
-      //     //       index == 1 ? 1 : 0,
-      //     //     ),
-      //     //     duration: Duration(seconds: index == 2 ? 0 : 1))
-      //     .fade(delay: Duration(milliseconds: 400)),
-
-      // buildImageWidget(
-      //     index: index,
-      //     imagePath: index == 0 ? OBman : transparent,
-      //     top: index == 0 ? MediaQuery.of(context).size.height / 3.5 : 0,
-      //     left: index == 0 ? MediaQuery.of(context).size.width / 1.495 : 0,
-      //     height: index == 0 ? 275 : 0,
-      //     context: context),
-    ],
-  );
+  return Center(child: SizedBox(height: 426,width: 380,child: AvailableCreditComponent(index: index,)));
 }
 Widget buildImageWidget(
     {int? index,
